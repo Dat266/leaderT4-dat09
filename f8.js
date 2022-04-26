cebf29d3f08f1c4669ee2113d9f9efd9bc2d58b6
Dat266 <datdev266@gmail.com>
2022-04-26 20:38:29 +0700

    add file

---
+++ f8.js
@@ -0,0 +1,18 @@
+let person = {
+    fullName: 'Pham Tuan Dat',
+    age: 25,
+    date: 1997,
+    address: 'Hải Phòng'
+
+}
+
+
+console.log(person);
+
+
+
+
+setTimeout(function() {
+    let nameValue = prompt('Nhập tên của bạn: ');
+    console.log(nameValue);
+}, 2000);
